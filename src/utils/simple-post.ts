f8556dc109c6fca8152cce5d61b61baea2928d51
Spencer Sokol <spencer@idkwtm.com>
2025-07-09 16:55:10 -0400

    Dependency updates for SimplePosts

--- src/utils/simple-post.ts
+++ src/utils/simple-post.ts
@@ -1,20 +1,39 @@
-import { SimplePostFactory, ISimplePost, ISimplePostMetaData } from "@idkwtm/simple-posts";
+import { SimplePostFactory, SimplePost, ISimplePost, ISimplePostMetaData } from "@idkwtm/simple-posts";
 
-export interface MySimplePostMetaData extends ISimplePostMetaData {
-    description: string
+export interface IMySimplePostMetaData extends ISimplePostMetaData {
+    youTubeURL: string
 }
 
-export interface MySimplePost extends MySimplePostMetaData, ISimplePost {}
+export interface IMySimplePost extends IMySimplePostMetaData, ISimplePost {
+    getYouTubeEmbedCode(): string;
+}
+
+export class MySimplePost extends SimplePost implements IMySimplePost {
+
+    youTubeURL: string;
+
+    constructor(data: IMySimplePost) {
+        super(data);
+        this.youTubeURL = data.youTubeURL;
+    }
+
+    getYouTubeEmbedCode() : string {
+        if (!this.youTubeURL)
+            return '';
+
+        return `<iframe width="560" height="315" src="${this.youTubeURL}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+    };
+}
 
 export class MySimplePostFactory extends SimplePostFactory {
 
-    createPost(meta: MySimplePostMetaData, content: string): MySimplePost {
+    createPost(meta: IMySimplePostMetaData, content: string): IMySimplePost {
         
-        const post = super.createPost(meta, content) as MySimplePost;
+        const post = super.createPost(meta, content) as IMySimplePost;
 
-        post.description = meta.description ?? '';
+        post.youTubeURL = meta.youTubeURL ?? '';
 
-        return post;
+        return new MySimplePost(post);
     }
     
 }
