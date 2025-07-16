ee77760d6bd0c03e2d7fcc9fe6ade3f206620457
Spencer Sokol <spencer@idkwtm.com>
2025-07-16 18:36:58 -0400

    RSS and sitemap additions

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
             </main>
             <footer className='container mx-auto py-4 flex-none text-xs font-thin'>
                 <section className='py-4 flex justify-center'>
-                    <a href="/rss.xml">RSS</a> &nbsp; | &nbsp; <s>sitemap</s>
+                    <a href="/rss.xml">RSS</a> &nbsp; | &nbsp; <a href="/sitemap.xml">sitemap</a>
                 </section>
                 <section className='py-1 flex justify-center uppercase'>
                     &copy; { new Date().getFullYear() } Spencer R. Soko<span className='rotate-y-180'>l</span>
